b3b5ef3e84c260f1128fd87c049ef92fecf6aa1a
Nelsonrrj <nelsonrodriguezj@gmail.com>
2025-06-07 12:18:17 -0400

    feat(environment): integrate vlucas/phpdotenv for environment variable management and update Docker configuration

--- public/index.php
+++ public/index.php
@@ -2,4 +2,9 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
 require_once __DIR__ . '/router.php';
--- src/Infrastructure/DI/Definitions/Infrastructure.php
+++ src/Infrastructure/DI/Definitions/Infrastructure.php
@@ -11,10 +11,20 @@ use DI\Container;
 use Doctrine\ORM\EntityManagerInterface;
 
 return [
-  DatabaseConnection::class => function () {
-    $config = require __DIR__ . '/../../../../config/database.php';
+  DatabaseConnectionParams::class => function () {
+    return new DatabaseConnectionParams(
+      driver: getenv('DB_DRIVER'),
+      host: getenv('DB_HOST'),
+      port: (int) getenv('DB_PORT'),
+      dbname: getenv('DB_DATABASE'),
+      user: getenv('DB_USERNAME'),
+      password: getenv('DB_PASSWORD'),
+    );
+  },
+
+  DatabaseConnection::class => function (Container $container) {
     $entitiesPath = __DIR__ . '/../../../../src/Infrastructure/Persistence/Entities';
-    return new DatabaseConnection(new DatabaseConnectionParams(...$config), $entitiesPath);
+    return new DatabaseConnection($container->get(DatabaseConnectionParams::class), $entitiesPath);
   },
 
   EntityManagerInterface::class => function (Container $container) {
